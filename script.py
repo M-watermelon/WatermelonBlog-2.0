ce2e48a89525e5c77965f29ab69c54686cc8262a
M-watermelon <mwatermelon20@gmail.com>
2022-05-06 09:50:59 -0500

    main

---
+++ script.py
@@ -0,0 +1,22 @@
+import tkinter
+from tkinter import *
+root = tkinter.Tk()
+
+
+#functions to be used in the game
+def setup(): #setup function for game
+    canvas = tkinter.Canvas(root, bg="white", height=600, width=600)
+    title = Label(root, text="Snake Game" , font=("Arial", 30))
+    title.pack()
+    canvas.create_oval(60,60,210,210, fill = "purple")
+    button = Button (root,bg="#c1eb9d", text = "Quit", width = 2, height = 2, command=root.destroy )
+    button.place(x=500, y=2)
+    canvas.pack()
+    root.mainloop()
+
+# The program that is run, calling of functions is below:
+
+setup()
+
+
+
